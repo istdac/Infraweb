bd0ed468bfdb9ce2f7ec4e1ff49693ce052cad6d
DiegoCadena <diego@graywolf.co>
2022-03-22 11:59:10 -0700

    Initial new api integration

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
     console.log("dom ready")
+    
     $('#btnan').on('click', function() {
         let idStr = $('#pubtxt').val();
         // let data = T.analize(ID);
@@ -8,16 +9,54 @@ $(document).ready(function () {
         console.log(ID);
         $.ajax({
             type: 'get',
-            url: 'https://mighty-cliffs-43835.herokuapp.com/tweets',
+            url: 'http://localhost:3100/Singletweet',
             data: {
                 id : ID
             },
             success: function(res) {
-                console.log(res)
-                $('#resultados').text(res.data[0].text);
+                console.log("res:");
+                console.log(res.data);
+                $('#resultados').text(res.data.text);
+                
             }
         })
-    });
+    });//getTweet
+    $('#btnUserSearch').on('click',function(){
+        let name = $('#userNameSearch').val();
+        console.log('name:'+name);
+        $.ajax({
+            type: 'get',
+            url : 'http://localhost:3100/userSearch',
+            data :{
+                username:name
+            },
+            success: function(res){
+                console.log('res usernameSearch:');
+                console.log(res);
+                $('#resarea').html('Usuario: '+ res.data.id+
+                '<br/> Nombre: '+res.data.name+
+                '<br/> Nombre de usuario: '+res.data.username);
+            }
+        });
+    });//function buscar usuario
+    $('#btnPostTweet').on('click',function(){
+        let content = $('#postTwee').val();
+        $.ajax({
+            type:'get',
+            url: 'http://localhost:3100/tweets',
+            data:{
+                text : content
+            },
+            success:function(res){
+                console.log("res post tweet:");
+                console.log(res.data);
+                $('#resultados').text(
+                    'Tweet creado con id:'+res.data[0]
+                );
+            }
+        });
+    });//function post tweet
+    
 });
 
 function separateId(str) {
@@ -25,4 +64,5 @@ function separateId(str) {
     id = id[id.length-1].split('?')[0];
     return id;
 }
+
 
--- js/page.js
+++ js/page.js
@@ -1,26 +1,30 @@
 const express = require('express');
 const Twitter = require('twitter-v2');
+const { TwitterApi } = require('twitter-api-v2');
+const tweetJsonToHtml = require('tweet-json-to-html');
 const app = express();
-let port = process.env.PORT;
+/* let port = process.env.PORT;
 if (port == null || port == "") {
   port = 8000;
 }
 app.listen(port);
-console.log("port:"+port);
-//app.listen(3100, () => console.log('Server running'));
+console.log("port:"+port); */
+app.listen(3100, () => console.log('Server running'));
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
   next();
 });
+const apponlyClient = new TwitterApi('AAAAAAAAAAAAAAAAAAAAAOrcWQEAAAAAVd8xl8wTnoFPfyPZnbicfv5JuHM%3D0gXK89sM7HshxaRn8wHacElUPCBgXVEIPp8bQ8trCCIh4QLrlV');
+const v2Client = apponlyClient.v2;
 const client = new Twitter({
     bearer_token: 'AAAAAAAAAAAAAAAAAAAAAOrcWQEAAAAAVd8xl8wTnoFPfyPZnbicfv5JuHM%3D0gXK89sM7HshxaRn8wHacElUPCBgXVEIPp8bQ8trCCIh4QLrlV'
 });
 console.log("en page");
 
-app.get('/tweets', (req,res) => {
-  //console.log(req);
+app.get('/Singletweet', (req,res) => {
+/*   //console.log(req);
     client
       .get('tweets', {ids: req.query.id})
       .then(tweet => {   
@@ -28,5 +32,37 @@ app.get('/tweets', (req,res) => {
       })
       .catch(error => {
         res.send(error);
+      }); */
+      v2Client.singleTweet(req.query.id).then(twwwt =>{
+        res.json(twwwt);
+      }).catch(error =>{
+        res.send(error)
       });
 });
+app.get('/tweets',(req,res)=>{
+    v2Client.tweet(req.query.text);
+});
+app.get('/userTL', (req,res) => {
+  /*   //console.log(req);
+      client
+        .get('tweets', {ids: req.query.id})
+        .then(tweet => {   
+          res.json(tweet);
+        })
+        .catch(error => {
+          res.send(error);
+        }); */
+        v2Client.userTimeline(req.query.id).then(twwwt =>{
+          res.json(twwwt);
+        }).catch(error =>{
+          res.send(error)
+        });
+  });
+  app.get('/userSearch',(req,res) =>{
+      v2Client.userByUsername(req.query.username).then(user =>{
+        let html  = tweetJsonToHtml(res.json(user))
+        res.send(html);
+      }).catch(error =>{
+        res.send(error);
+      });
+  });
